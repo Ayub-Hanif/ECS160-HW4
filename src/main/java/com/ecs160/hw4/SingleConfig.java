176c2ec106ca03d55074f61be43996b4dc67ae65
Ayub <ayubhanif06@gmail.com>
2025-03-20 15:36:43 -0700

    Refactor SingleConfig class for improved Singleton implementation and code cleanliness

--- src/main/java/com/ecs160/hw4/SingleConfig.java
+++ src/main/java/com/ecs160/hw4/SingleConfig.java
@@ -3,24 +3,18 @@ package com.ecs160.hw4;
 /**
  * Singleton class to hold configuration options for the SocialAnalyzer.
  * We want exactly one instance of our configuration class to exist.
- * We make sure it is globally available to other classes such as our Analyzer
- * or your Visitors.
+ * We make sure it is globally available to other classes such as our Analyzer or your Visitors.
  */
 public class SingleConfig {
     private static SingleConfig instance;
     private boolean weighted;
     private String jsonFilePath;
 
-    private SingleConfig() {
-    }
+    private SingleConfig() { }
 
     public static SingleConfig getInstance() {
         if (instance == null) {
-            synchronized (SingleConfig.class) {
-                if (instance == null) {
-                    instance = new SingleConfig();
-                }
-            }
+            instance = new SingleConfig();
         }
         return instance;
     }
@@ -28,7 +22,6 @@ public class SingleConfig {
     public boolean isWeighted() {
         return weighted;
     }
-
     public void setWeighted(boolean weighted) {
         this.weighted = weighted;
     }
@@ -36,7 +29,6 @@ public class SingleConfig {
     public String getJsonFilePath() {
         return jsonFilePath;
     }
-
     public void setJsonFilePath(String jsonFilePath) {
         this.jsonFilePath = jsonFilePath;
     }
